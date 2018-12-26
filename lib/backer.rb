a631a58ed2f9ea1939eaed11f23516bc0a55b6c9
Marie Burns <mcburns2222@gmail.com>
2018-12-26 22:58:57 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,8 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(project)
-    @backed_projects << Project.new(project)
+  def back_project(Project.new)
+    @backed_projects << Project.new
   end
   
   
